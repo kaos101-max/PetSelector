2c46830d03b822f1ecaef276be812ebc84b588a3
kaos101-max <watanabedaisuke616@gmail.com>
2025-10-13 18:27:21 +0000

    IronWill

--- PetSelector.java
+++ PetSelector.java
@@ -1,1 +1,16 @@
+import java.util.*;
+public class PetSelector{
+    public static void main(String [] args){
+        //ask the user for input (name, season, and color)
+        //make sure the input is valid (Idiot proof!)
+        //make a deceision, tell the user what they get!
+        Scanner userInput = new Scanner (System.ln);
+        System.out.println("Enter your favorite color (Either red, blue, or green):");
+        String colors = scan.nextLine();
+        String season;
+        String name;
+        String pet;
 
+
+    }
+}
