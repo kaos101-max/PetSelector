bae2d62b698e988d90797d563bbeb5a729dbfaf0
kaos101-max <watanabedaisuke616@gmail.com>
2025-10-16 18:29:16 +0000

    Swarm

--- PetSelector.java
+++ PetSelector.java
@@ -1,16 +1,33 @@
+//Name: Diauske Watanabe
 import java.util.*;
 public class PetSelector{
     public static void main(String [] args){
         //ask the user for input (name, season, and color)
         //make sure the input is valid (Idiot proof!)
         //make a deceision, tell the user what they get!
-        Scanner userInput = new Scanner (System.ln);
+        Scanner userInput = new Scanner (System.in);
         System.out.println("Enter your favorite color (Either red, blue, or green):");
-        String colors = scan.nextLine();
-        String season;
-        String name;
+        String colors = userInput.nextLine();
+
+        System.out.println("Also, enter your favorite season (Either spring, summer, fall, or winter):");
+        String season = userInput.nextLine();
         
-        if (colors.toLowerCase() == "red"){
+        System.out.println ("Enter your name here:");
+        String name;
+
+        //Starter for a pet
+        String pet = "Pet Rock";
+        
+        if (colors.equals("red") && season.equals("winter")){
+            pet = "panda";
+        }
+        if (colors.equals("blue") && season.equals("fall")){
+            pet = "alligator";
+        }
+        if (colors.equals("green") && season.equals("winter")){
+            pet = "giraffe";
+        }
+            
         
         }
         
